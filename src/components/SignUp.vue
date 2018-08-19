1468efdd120d057fe94d754dadff0ec3e8c43650
Credwa <craigroe7@gmail.com>
2018-08-19 00:26:38 -0400

    initial commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <b-form class="sign-up" @submit.stop.prevent="onSubmit">
+      <b-form-group id="nameGroup"
+                    label="Name"
+                    label-for="name"
+                    label-text-align="right">
+        <b-form-input id="name"
+                      type="text"
+                      v-model.trim="form.name"
+                      required
+                      placeholder="Enter name">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="emailGroup"
+                    label="Email"
+                    label-for="email">
+        <b-form-input id="email"
+                      type="email"
+                      v-model.trim="form.email"
+                      required
+                      :state="validateEmail"
+                      placeholder="Enter email">
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group id="passwordGroup"
+                    label="Password"
+                    label-for="password">
+        <b-form-input id="password"
+                      type="password"
+                      v-model.trim="form.password"
+                      required
+                      placeholder="Enter password">
+        </b-form-input>
+      </b-form-group>
+      <b-button type="submit" class="center-it">Get started now</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SignUp',
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        password: '',
+      },
+    };
+  },
+  computed: {
+    validateEmail() : Boolean {
+      const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      let result = false;
+      if (emailReg.test(this.form.email)) {
+        result = true;
+      }
+      if (this.form.email.length === 0) {
+        result = true;
+      }
+      return result;
+    }
+  },
+  methods: {
+    onSubmit(evt : any) : void {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.sign-up {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex-direction: center;
+}
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="auth">
+    <sign-up></sign-up>
+  </div>
+</template>
+
+<script lang="ts">
+declare var require: any;
+import Vue from 'vue';
+import SignUp from '@/components/SignUp.vue';
+import Confirmation from '@/components/Confirmation.vue'
+
+export default Vue.extend({
+  name: 'Auth',
+  components: {
+    SignUp,
+    Confirmation
+  },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.auth {
+  height: 100vh;
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+</style>
